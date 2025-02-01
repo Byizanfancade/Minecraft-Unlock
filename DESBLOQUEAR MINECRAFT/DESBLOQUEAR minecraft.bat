41dca5fdcc00aa55cc34682c27720f36e318acfd
Byizanfancade <izanfancade@gmail.com>
2025-02-01 16:36:55 -0400

    Add files via upload

---
+++ DESBLOQUEAR MINECRAFT/DESBLOQUEAR minecraft.bat
@@ -0,0 +1,2 @@
+"ARCHIVO DE DESBLOQUEADOR\IObitUnlocker.exe" "C:\Windows\System32\Windows.ApplicationModel.Store.dll" /delete & "ARCHIVO DE DESBLOQUEADOR\IObitUnlocker.exe" "C:\Windows\SysWOW64\Windows.ApplicationModel.Store.dll" /delete & "ARCHIVO DE DESBLOQUEADOR\cmd.lnk"
+
