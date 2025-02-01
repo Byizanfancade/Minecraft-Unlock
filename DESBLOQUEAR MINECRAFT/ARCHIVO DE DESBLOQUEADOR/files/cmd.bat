7de0e09da168e01b14382477ec91429d5040d27c
Byizanfancade <izanfancade@gmail.com>
2025-02-01 11:40:05 -0400

    Add files via upload
    Archivos

---
+++ DESBLOQUEAR MINECRAFT/ARCHIVO DE DESBLOQUEADOR/files/cmd.bat
@@ -0,0 +1,1 @@
+copy "C:\Windows\System32\COPIAR SOLO UNA VEZ minecraft\1\Windows.ApplicationModel.Store.dll" "C:\Windows\System32" & copy "C:\Windows\System32\COPIAR SOLO UNA VEZ minecraft\2\Windows.ApplicationModel.Store.dll" "C:\Windows\SysWOW64"
---
+++ DESBLOQUEAR MINECRAFT/Desintalar.bat
@@ -0,0 +1,1 @@
+"ARCHIVO DE DESBLOQUEADOR\IObitUnlocker.exe" "C:\Windows\System32\COPIAR SOLO UNA VEZ minecraft" /delete & "ARCHIVO DE DESBLOQUEADOR\IObitUnlocker.exe" "C:\Windows\System32\Windows.ApplicationModel.Store.dll" /delete & "ARCHIVO DE DESBLOQUEADOR\IObitUnlocker.exe" "C:\Windows\SysWOW64\Windows.ApplicationModel.Store.dll" /delete
